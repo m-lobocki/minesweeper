1421d44a33bec92128ce80f15779fcb6cddc604a
Mateusz Łobocki <mateusz.lobocki@blue-veery.com>
2019-07-14 16:26:40 +0200

    add window icon

--- src/dsw/Main.java
+++ src/dsw/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,6 +15,7 @@ public class Main extends Application {
         primaryStage.setTitle("Minesweeper");
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("gui/main.css").toExternalForm());
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("gui/mine.png")));
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
